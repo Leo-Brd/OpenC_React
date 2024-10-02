13e7642bde253c73105d41d75024b432bcadaa6e
Leo-Brd <leobordet.pro@gmail.com>
2024-10-02 16:25:37 +0200

    P2C4 begin

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,27 +1,34 @@
 import { plantList } from '../datas/plantList'
-
+import CareScale from './CareScale'
+import '../styles/ShoppingList.css'
+import '../styles/PlantItem.css'
 
 function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-    const categories = plantList.reduce((acc, plant) => {
-        return acc.includes(plant.category) ? acc : acc.concat(plant.category);
-    }, []);
-
-    return (
-        <div>
-            <ul>
-                {categories.map((cat) => (
-                    <li key={`${cat}`}>{cat}</li>
-                ))}
-            </ul>
-
-            <ul>
-                {plantList.map((plant) => (
-                    <li key={`${plant.id}`}>{plant.name}</li>
-                ))}
-            </ul>
-        </div>
-    )
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.isBestSale && <span>🔥</span>}
+						{plant.name}
+						<CareScale careType='water' scaleValue={plant.water} />
+						<CareScale careType='light' scaleValue={plant.light} />
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
