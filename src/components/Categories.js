ef602d9d9a8255c1b66db34bd622a6e22a331386
Leo-Brd <leobordet.pro@gmail.com>
2024-10-04 16:03:45 +0200

    P3C3 begin

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,30 +1,23 @@
-import { plantList } from '../datas/plantList'
+import '../styles/Categories.css'
 
-function Categories({ category, changeCategory }) {
-    const categories = plantList.reduce(
-		(acc, plant) =>
-			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-		[]
+function Categories({ setActiveCategory, categories, activeCategory }) {
+	return (
+		<div className='lmj-categories'>
+			<select
+				value={activeCategory}
+				onChange={(e) => setActiveCategory(e.target.value)}
+				className='lmj-categories-select'
+			>
+				<option value=''>---</option>
+				{categories.map((cat) => (
+					<option key={cat} value={cat}>
+						{cat}
+					</option>
+				))}
+			</select>
+			<button onClick={() => setActiveCategory('')}>Réinitialiser</button>
+		</div>
 	)
-
-    return (
-        <div id='lmj-categories-select'>
-            <select 
-                onChange={(event) => changeCategory(event.target.value)}
-                value = {category}
-            >
-
-                <option value='all'>Tous</option>
-                {categories.map((cat) => (
-                    <option key={cat} value={cat}>{cat}</option>
-                ))}
-
-		    </select>
-
-            <button onClick={() => changeCategory('all')}>Reinitialiser</button>
-
-        </div>
-    )
 }
 
 export default Categories
